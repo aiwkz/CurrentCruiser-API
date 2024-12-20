c63df07f2f88bfebc81449d539b4903f2b8aa79d
aiwkz <federicoj.pp@gmail.com>
2024-12-20 18:12:57 +0100

    Fixes database import on index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './database.js';
+import connectDB from '../config/database.js';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
